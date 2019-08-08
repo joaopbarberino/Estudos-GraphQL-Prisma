bdc08d0ccce681cd302eb512c67ffadb628a397e
João Pedro Barberino Silva <joao.p.barberino@gmail.com>
2019-08-08 17:05:37 -0300

    finish lesson 53, on section 5

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,16 +1,71 @@
 import { Prisma } from 'prisma-binding';
+import { parseTypeReference } from 'graphql/language/parser';
 
 const PRISMA = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466',
 });
 
-PRISMA.query.users(null, '{ id name email posts { id title } }').then((data) => {
-    //console.log(JSON.stringify(data, undefined, 2));
-});
+// PRISMA.query.users(null, `
+//     { 
+//         id 
+//         name 
+//         email 
+//         posts { 
+//             id 
+//             title 
+//         }
+//     } 
+// `).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
 
-//console.log(PRISMA.query)
+// //console.log(PRISMA.query)
+// const fetchComments = `
+// {
+//     id
+//     text
+//     author {
+//         id
+//         name
+//     }
+// }
+// `;
 
-PRISMA.query.comments(null, '{ id text author { id name } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 4));
-});
+// PRISMA.query.comments(null, fetchComments).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 4));
+// });
+
+// PRISMA.mutation.createPost({
+//     data: {
+//         title: "Challeng post 2",
+//         body: "",
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: "cjykgvzhr00cs08031019iso9"
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then((data) => {
+//     console.log(data);
+//     return PRISMA.query.users(null, '{ id name posts { id title } }')
+// }).then(data => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
+
+// PRISMA.mutation.updatePost({
+//     data: {
+//         body: "existsssss",
+//         published: true
+//     },
+//     where: {
+//         id: "cjz338fe4001n0803udecr95n"
+//     }
+// }, '{ id title body published }').then(data => {
+//     console.log(data);
+//     return PRISMA.query.posts(null, '{ id title body published }')
+// }).then(data => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
+
