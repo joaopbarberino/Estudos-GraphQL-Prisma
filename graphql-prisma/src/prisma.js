b0b150fa388438dc324719436f70cde20d2a8f25
João Pedro Barberino Silva <joao.p.barberino@gmail.com>
2019-08-19 10:53:46 -0300

    Section 5, class 55

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -6,66 +6,67 @@ const PRISMA = new Prisma({
     endpoint: 'http://localhost:4466',
 });
 
-// PRISMA.query.users(null, `
-//     { 
-//         id 
-//         name 
-//         email 
-//         posts { 
-//             id 
-//             title 
-//         }
-//     } 
-// `).then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2));
+// 1. Create a new post
+// 2. Fetch all of the info the user (author)
+const createPostForUser = async (authorId, data) => {
+    const userExists = await PRISMA.exists.User({ id: authorId });
+    
+    if (!userExists) throw new Error('User not found!');
+    // 1.
+    const post = await PRISMA.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+        // 2.
+    }, '{ id author { id name posts { id title published } } }');
+    
+    return post.author;
+}
+
+// createPostForUser('cjykgb3gb000b0803agt3uxa2', {
+//     title: 'HQs super boas',
+//     body: 'The Dark Knights Returns',
+//     published: true
+// }).then(user => {
+//     console.log(JSON.stringify(user, undefined, 4));
+// }).catch(error => {
+//     console.log(error);
+// })
+
+// 1. Update a post and get the author id back
+// 2. Fetch the user associated with the post and return it's data
+const updatePostForUser = async (postId, data) => {
+    const postExists = await PRISMA.exists.Post({ id: postId });
+    
+    if(!postExists) throw new Error('Post not found');
+    // 1.
+    const post = await PRISMA.mutation.updatePost({
+        where: {
+            id: postId
+        },
+        data: {
+            ...data,
+        }
+        // 2.
+    }, '{ id author { name email posts { id title published } } }');
+    
+    return post.author;
+}
+
+// updatePostForUser('cjz462kkn00gc0903nunasub5', {
+//     title: 'Filmes incrivelmente bons',
+//     body: 'Interstellar'
+// }).then(post => {
+//     console.log(JSON.stringify(post, undefined, 4));
+// }).catch(error => console.log(error.message));
+
+// PRISMA.exists.Post({
+//     id: "cjz462kkn00gc0903nunasub5"
+// }).then(exists => {
+//     console.log(exists);
 // });
-
-// //console.log(PRISMA.query)
-// const fetchComments = `
-// {
-//     id
-//     text
-//     author {
-//         id
-//         name
-//     }
-// }
-// `;
-
-// PRISMA.query.comments(null, fetchComments).then((data) => {
-//     console.log(JSON.stringify(data, undefined, 4));
-// });
-
-// PRISMA.mutation.createPost({
-//     data: {
-//         title: "Challeng post 2",
-//         body: "",
-//         published: false,
-//         author: {
-//             connect: {
-//                 id: "cjykgvzhr00cs08031019iso9"
-//             }
-//         }
-//     }
-// }, '{ id title body published }').then((data) => {
-//     console.log(data);
-//     return PRISMA.query.users(null, '{ id name posts { id title } }')
-// }).then(data => {
-//     console.log(JSON.stringify(data, undefined, 2));
-// });
-
-// PRISMA.mutation.updatePost({
-//     data: {
-//         body: "existsssss",
-//         published: true
-//     },
-//     where: {
-//         id: "cjz338fe4001n0803udecr95n"
-//     }
-// }, '{ id title body published }').then(data => {
-//     console.log(data);
-//     return PRISMA.query.posts(null, '{ id title body published }')
-// }).then(data => {
-//     console.log(JSON.stringify(data, undefined, 2));
-// });
-
